a7de24fd6b7bf53c2886ca12541f891e67fa926c
luztherose <luzdelarosaramirez@gmail.com>
2020-10-20 11:07:04 -0400

    code refactor

--- JS/script.js
+++ JS/script.js
@@ -8,55 +8,55 @@ function nextQuestion() {
     if (currentPosition == 12) {
         // We are in the last question
         // Hide last question
-        var lastQuestion = document.getElementById("Q12");
+        const lastQuestion = document.getElementById("Q12");
         lastQuestion.classList.add("hide");
         // Hide next button
-        var nextButton = document.getElementById("next");
+        const nextButton = document.getElementById("next");
         nextButton.classList.add("hide");
         // Hide Previous QuestionButton
-        var buttonPreviousQuestion = document.getElementById("previousButton");
+        const buttonPreviousQuestion = document.getElementById("previousButton");
         buttonPreviousQuestion.classList.add("hide");
         // Hide the rules and questions indicator
-        var headerLabel = document.getElementById("headerLabel");
+        const headerLabel = document.getElementById("headerLabel");
         headerLabel.classList.add("hide");
 
         calculateScore();
 
         // Show the score
-        var userScore = document.getElementById("score");
+        const userScore = document.getElementById("score");
         userScore.classList.remove("hide");
         // Give the total score
         userScore.innerHTML = "Your Score is " + score + " of 12";
         // Calculate user level
         if  (score == 12) {
-            var winner = document.getElementById("img-winner");
+            const winner = document.getElementById("img-winner");
             winner.classList.remove("hide");
         }else if (score == 10 || score == 11) {
-            var secondWinner = document.getElementById("secondWinner");
+            const secondWinner = document.getElementById("secondWinner");
             secondWinner.classList.remove("hide");
         } else if (score == 8 || score == 9)   {
-            var thirdWinner = document.getElementById("thirdWinner");
+            const thirdWinner = document.getElementById("thirdWinner");
             thirdWinner.classList.remove("hide");
         }else {
-            var noWinner = document.getElementById("noPlace");
+            const noWinner = document.getElementById("noPlace");
             noWinner.classList.remove("hide");
         }
         // Show Try Again Button
-        var button = document.getElementById("tryAgainButton");
+        const button = document.getElementById("tryAgainButton");
         button.classList.remove("hide");
         // See All Answers
-        var buttonSeeAnswer = document.getElementById("seeAnswersButton");
+        const buttonSeeAnswer = document.getElementById("seeAnswersButton");
         buttonSeeAnswer.classList.remove("hide");
     } else {     
         //Enableling previous buttom
         document.getElementById("previousButton").disabled = false;
         // Hide the current question
-        var currentQuestion = document.getElementById("Q" + currentPosition);
+        const currentQuestion = document.getElementById("Q" + currentPosition);
         currentQuestion.classList.add("hide");
         // Chage the current question 
         currentPosition = currentPosition + 1
         
-        var currentQuestionCount = document.getElementById("currentQuestionCount");
+        const currentQuestionCount = document.getElementById("currentQuestionCount");
         currentQuestionCount.innerHTML = currentPosition;
         
         // Show next question
@@ -78,11 +78,11 @@ function tryAgainButton(){
 function calculateScore() {
     // Calculating the score for each question
     for(var i = 1; i <= 12; i = i + 1) {
-        var currentQuestion = document.getElementById("Q" + i);
+        const currentQuestion = document.getElementById("Q" + i);
         // User answer #Q1 :checked
-        var userSelection = currentQuestion.querySelector(":checked"); 
+        let userSelection = currentQuestion.querySelector(":checked"); 
         // Correct Answer #Q2 .rightAnswer
-        var correctAnswer = currentQuestion.querySelector(".rightAnswer");
+        let correctAnswer = currentQuestion.querySelector(".rightAnswer");
     
         // Is the user answer correct?
         if (userSelection == correctAnswer) {
@@ -96,14 +96,14 @@ function calculateScore() {
     // Create a function to go to the Previous Question
 function previousQuestion() { 
     // Hide Current Question 
-    var currentQuestion = document.getElementById("Q" + currentPosition);
+    const currentQuestion = document.getElementById("Q" + currentPosition);
     currentQuestion.classList.add("hide");
     // Chage the current question 
     currentPosition = currentPosition - 1
-    var currentQuestionCount = document.getElementById("currentQuestionCount");
+    const currentQuestionCount = document.getElementById("currentQuestionCount");
     currentQuestionCount.innerHTML = currentPosition;
     // Show Previous Question
-    var preQuestion = document.getElementById("Q" + currentPosition);
+    const preQuestion = document.getElementById("Q" + currentPosition);
     preQuestion.classList.remove("hide");  
     // If the current question is the first one hide the previousButton
     if (currentPosition  == 1) {
@@ -113,35 +113,25 @@ function previousQuestion() {
     // See all the questions with correct Answers
 function allQuestionsAnswers() {
     // Hide images 
-    var winner = document.getElementById("img-winner");
+    const winner = document.getElementById("img-winner");
         winner.classList.add("hide");
-    var noWinner = document.getElementById("noPlace");
+    const noWinner = document.getElementById("noPlace");
         noWinner.classList.add("hide");
-    var thirdWinner = document.getElementById("thirdWinner");
+    const thirdWinner = document.getElementById("thirdWinner");
         thirdWinner.classList.add("hide");
-    var secondWinner = document.getElementById("secondWinner");
+    const secondWinner = document.getElementById("secondWinner");
         secondWinner.classList.add("hide");
      // Show all the correct questions
-    document.getElementById("Q1").style.display="block";
-    document.getElementById("Q2").style.display="block";
-    document.getElementById("Q3").style.display="block";
-    document.getElementById("Q4").style.display="block";
-    document.getElementById("Q5").style.display="block";
-    document.getElementById("Q6").style.display="block";
-    document.getElementById("Q7").style.display="block";
-    document.getElementById("Q8").style.display="block";
-    document.getElementById("Q9").style.display="block";
-    document.getElementById("Q10").style.display="block";
-    document.getElementById("Q11").style.display="block";
-    document.getElementById("Q12").style.display="block";
+    for(let i = 1; i <= 12; i++) {
+        document.getElementById("Q" + i).style.display="block";
+        
+    }
     // Show the correct answer with a green background
-
     const correctAnswers = document.getElementsByClassName("labelRightAns");
 
     for(let i = 0; i < correctAnswers.length; i++) {
         correctAnswers[i].classList.add("lightgreenColor");
     }
-    
 }
     
 let bodyBackground = document.getElementsByTagName("body")[0];
@@ -160,7 +150,6 @@ function changeBackgroundColor() {
 function changeBackgroundToDefault() {
     toggleButton.innerHTML = "Change Background Color";
     bodyBackground.style.backgroundColor = "rgb(255, 222, 219) ";
-    //location.reload();
 }
 toggleButton.addEventListener("click", () => {
     if (h2Customize.style.color != "navy") {
